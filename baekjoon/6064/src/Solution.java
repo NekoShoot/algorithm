f6828c4e907e6c4d3188bd871dd82a04e7bf9d3c
NekoShoot <honggyunjo1109@gmail.com>
2024-07-17 09:00:09 +0900

    First Commit

---
+++ baekjoon/6064/src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ baekjoon/9095/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 테스트 케이스 T
+		int T = sc.nextInt();
+		
+		// 테스트 케이스 수 만큼 반복
+		for (int test_case = 1; test_case < T + 1; test_case++) {
+			// 정수 0 <= n < 11
+			int n = sc.nextInt();
+			
+		}
+
+	}
+
+}
