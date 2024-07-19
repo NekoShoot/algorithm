782dd83fee261d06136bd07ce93215fefbabf759
NekoShoot <honggyunjo1109@gmail.com>
2024-07-20 00:20:04 +0900

    1244 done but not pass

--- baekjoon/1244/src/Main.java
+++ baekjoon/1244/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
 public class Main {
 
@@ -26,61 +26,139 @@ public class Main {
 		 * (반복문을 돌릴 때 반복 횟수를 저장할 값을 만들기 cnt = 1; -> 
 		 * 이걸 활용해서 인덱스 조절해서 탐색하고 -> 
 		 * 멈췄을 때 횟수를 확인해서 y-1-cnt ~ y-1+cnt 범위 사이에 있는 스위치들 상태 바꾸기)
-		 */ 
+		 */ 		  
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
+		// 길이라 인덱스가 아님!!!
 		int switchRows = n < 20 ? n : 20;
 		// 20이면 1줄, 21이면 2줄, 99면 5줄, 100이면 5줄		
-		int switchCols = ((n - 1) / 20) + 1;		
-
-		
+		int switchCols = ((n - 1) / 20) + 1;					
+				
+		// 23이면 (2, 1)
 		// 스위치 상태 저장
-		int[][] switchArr = new int[switchRows][switchCols];
-		for(int i = 0; i < switchRows; i++) {
-			for(int j = 0; j < switchCols; j++) {
-				switchArr[i][j] = sc.nextInt();
+		int[][] switchArr = new int[switchCols][switchRows];
+out:	for(int i = 0; i < switchCols; i++) {
+			for(int j = 0; j < switchRows; j++) {
+				if(i == switchCols - 1 && j >= (n % 20)) {					
+					break out;
+				} 
+				
+				switchArr[i][j] = sc.nextInt();	
 			}
-		}
+		}		
 		
-		int studentNum = sc.nextInt();
+		int studentNum = sc.nextInt();		
 		
 		// 학생 수 만큼 반복 시행
-		//for(int i = 0; i < studentNum; i++) {			
-			int gender = sc.nextInt();
+		for(int i = 0; i < studentNum; i++) {			
+			int gender = sc.nextInt();			
 			
 			// case 1. 남자일 때
 			if(gender == 1) {
 				// 타겟 스위치 위치
-				int targetSwitch = sc.nextInt();				
+				int targetSwitch = sc.nextInt();
+				
 				// 스위치 조작 개수
-				int numberOfChangingSwitch = n / targetSwitch;				
+				int numberOfChangingSwitch = n / targetSwitch;	
 				
 				// 타겟 스위치 위치의 배수를 만들면서 스위치 상태 확인 후 변경!
-				for(int j = 0; j < numberOfChangingSwitch; j++) {
-					int multiplied = j * targetSwitch;
-					// 스위치 위치를 rows, cols로 가공
-					 
-					
-					
-				}
+				for(int j = 1; j < numberOfChangingSwitch + 1; j++) {
+					int multiplied = j * targetSwitch;					
+					// 스위치 위치를 rows, cols로 가공		
+					// 인덱스라 위에 switchRows,Cols와는 다름!
+					int targetRows = ((multiplied - 1) / 20);
+					int targetCols = multiplied <= 20 ? multiplied - 1 : (multiplied - (20 * (i + 1)) - 1);
+																			
+					// 0이면 1로 1이면 0으로
+					if(switchArr[targetRows][targetCols] == 0) switchArr[targetRows][targetCols] = 1;	
+						else if(switchArr[targetRows][targetCols] == 1) switchArr[targetRows][targetCols] = 0;
+				}			
+							
+				System.out.println("남자의 결과는");
+				System.out.println(Arrays.toString(switchArr[0]));
 				
-				
-				
-				
-				
-			} else { 
+			} else if(gender == 2) { 
 			//case 2. 여자일 때
+				int targetSwitch = sc.nextInt(); 				
+				
+				// 1(0, 0) 20(0, 19) 21(1, 0) 99(4, 18) 100(4, 19)
+				int targetRows = ((targetSwitch - 1) / 20);
+				int targetCols = targetSwitch < 20 ? targetSwitch - 1 : (targetSwitch - 1 % 20);				
+								
+				System.out.println("여자 동작 중");
+				System.out.println(targetRows + ", " + targetCols);
+				
+				// target이 0번째거나 19번째 인덱스이면 종료
+				if(targetCols == 0 || targetCols == 19) {
+					if(switchArr[targetRows][targetCols] == 0) switchArr[targetRows][targetCols] = 1;
+						else if(switchArr[targetRows][targetCols] == 1) switchArr[targetRows][targetCols] = 0;					
+					continue;
+				}								
+				
+				int cnt = 1;
+				while (true) {
+					int moveBack = switchArr[targetRows][targetCols - cnt];
+					int moveForward = switchArr[targetRows][targetCols + cnt];
+					
+					// 좌우대칭이면
+					if(moveBack == moveForward) {
+						// targetCols - cnt == 0 || targetCols + cnt == 19일때
+						// targetCols - cnt ~ targetCols + cnt 탐색 후 스위치 바꾸고 종료
+						int sub = 2 * cnt + 1; // 정지했을 때의 좌우대칭인 전체 스위치 개수
+						if(moveBack == 0 || moveForward == 19) {
+							for(int j = 0; j < sub; j++) {
+								if(switchArr[targetRows][targetCols - cnt + j] == 0) switchArr[targetRows][targetCols - cnt + j] = 1;
+									else if(switchArr[targetRows][targetCols - cnt + j] == 1) switchArr[targetRows][targetCols - cnt + j] = 0;
+							}			
+							
+							break;
+						}
+						
+						cnt++;
+						continue;
+						
+					} else { //좌우대칭이 아니면 target부터 targetCols - (cnt - 1) ~ targetCols + (cnt - 1)까지 탐색 후 스위치 바꾸고 종료 
+						int sub = 2 * (cnt - 1) + 1; // 정지했을 때의 좌우대칭인 전체 스위치 개수
+						for(int j = 0; j < sub; j++) {
+							if(switchArr[targetRows][targetCols - (cnt - 1) + j] == 0) switchArr[targetRows][targetCols - (cnt - 1) + j] = 1;
+								else if(switchArr[targetRows][targetCols - (cnt - 1) + j] == 1) switchArr[targetRows][targetCols - (cnt - 1) + j] = 0;
+						}					
+												
+						break;
+					}
+				}
 				
 				
 				
 			}				
 				
-
-		//}
-						
+			
+		}
+		System.out.println("최종 스위치 상태는");
+		System.out.println(Arrays.toString(switchArr[0]));		
 		
+		
+out:	for(int i = 0; i < switchCols; i++) {
+			for(int j = 0; j < switchRows; j++) {
+				if(i == switchCols - 1 && j >= (n % 20)) {					
+					break out;
+				} 
+				
+				if(j < switchRows - 1) {				
+					System.out.print(switchArr[i][j] + " ");					
+				} else if(j == switchRows - 1 && i < switchCols - 1) {					
+					System.out.print(switchArr[i][j]);
+					System.out.println("");
+				} else {
+					System.out.print(switchArr[i][j]);
+				}
+					
+			}
+		}		
+
+
 
 	}
 
