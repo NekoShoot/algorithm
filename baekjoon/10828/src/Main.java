8b58d1e9ce9196ea30d79acaa2a11d2f1e8cbde3
NekoShoot <honggyunjo1109@gmail.com>
2024-07-20 20:23:53 +0900

    make directory of new problem

---
+++ baekjoon/10828/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Stack
+		// push X -> x를 스택에 넣기
+		// pop -> 가장 위에 있는 정수를 빼고 그 수를 출력. 정수가 아예 없으면 -1 출력
+		// size -> 스택에 들어있는 정수 개수 출력
+		// empty -> 비어있으면 1 아니면 0
+		// top -> 스택의 가장 위에 있는 정수 출력. 정수가 아예 없으면 -1
+		
+		// 1 <= N <= 1만 명령의 수
+		// 1 <= X <= 10만
+
+	}
+
+}
