588a146d16ff396eae976f7ee2b05a2db085fe96
NekoShoot <honggyunjo1109@gmail.com>
2024-08-05 23:31:57 +0900

    mkdir 10163

---
+++ baekjoon/10163/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		// 색종이 (B1) 문제
+		/*
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
