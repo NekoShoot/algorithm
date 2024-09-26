ccb03447154f29fc547f1b77f631fe2ca1d6aad6
NekoShoot <honggyunjo1109@gmail.com>
2024-09-26 21:12:51 +0900

    1197 kruskal

--- baekjoon/1197_minimumSpanningTree/src/Main.java
+++ baekjoon/1197_minimumSpanningTree/src/Main.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static boolean[] visited;
+    
     static class Edge implements Comparable<Edge> {
         int nodeA; // 나
         int nodeB; // 너
@@ -21,7 +23,7 @@ public class Main {
     }
 
     // 공백을 건너 뛰고 유효한 줄만 읽기
-    public static String validateLine(BufferedReader br) throws IOException {
+    static String validateLine(BufferedReader br) throws IOException {
         String line;
         while((line = br.readLine()) != null) {
             line = line.trim();
@@ -41,6 +43,8 @@ public class Main {
         int V = Integer.parseInt(st.nextToken()); // 정점의 개수
         int E = Integer.parseInt(st.nextToken()); // 간선의 개수
 
+        visited = new boolean[V+1]; // 방문 배열 초기화
+        
         // 인접 리스트로 관리
         List<Edge>[] adjList = new ArrayList[V+1]; // 1번 노드부터 시작
 
@@ -66,19 +70,18 @@ public class Main {
         }
 
         PriorityQueue<Edge> pq = new PriorityQueue<>(); // 뽑은 간선의 가중치를 저장할 우선순위 큐
-        // 비트마스킹으로 방문 체크
-        int visited = 1 << 1; //
+        visited[1] = true;
         int ans = 0; // 정답 기록
         int pick = 1; // 뽑은 노드 개수
 
         pq.addAll(adjList[1]); // 1번 노드의 인접 노드를 전부 우선순위 큐에 넣음(가중치 기준 우선순위)
         while(pick != V) { // V개의 노드를 전부 보면 끝
             Edge e = pq.poll(); // 나(A)를 기준으로 가장 작은 가중치를 가진 간선 뽑아서 인접 노드(B) 확인하기
-            if((visited & (1 << e.nodeB)) != 0) continue; // 도착지가 이미 방문했던 노드라면 패스
+            if(visited[e.nodeB]) continue; // 도착지가 이미 방문했던 노드라면 패스
 
             // 방문한 적 없는 노드라면
             ans += e.weight; // 스패닝 트리에 거리 추가 (Greedy 알고리즘이라 최소값만 뽑아도 됨)
-            visited |= (1 << e.nodeB); // B노드 방문 처리
+            visited[e.nodeB] = true; // B노드 방문 처리
             pick++; // 뽑았다!
 
             // 뽑은 노드의 인접 노드를 넣음(A의 남은 인접 노드들과 B의 인접 노드들 중 가중치 기준 우선순위 결정)
